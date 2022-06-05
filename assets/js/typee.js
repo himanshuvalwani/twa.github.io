28885f5184ed4468d2d9625e5169c81c9574b6d6
TWA <104203176+himanshuvalwani@users.noreply.github.com>
2022-06-05 20:11:44 +0530

    Add files via upload

---
+++ assets/js/typee.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["AMC for Computer Hardware and Networking.", "CCTV Camera and Security Solutions.", "Network-solution", "Computer AMC Services In India.","AMC for Computer Hardware and Networking.","IT Consultants."],
+    typeSpeed: 100,
+    loop:true,
+   startDelay:1000,
+    });
